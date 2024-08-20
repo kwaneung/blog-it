a1dce848dad7a608bb9aa4434811e09258c0662b
kwaneung <kwaneung.kim@outlook.com>
2024-08-20 12:25:38 +0900

    feat: Optimize Package Imports 샘플 셋팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['@mui/material', '@mantine/hook'],
+  },
+};
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
