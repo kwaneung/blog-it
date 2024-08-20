5a5b8e32363673c17cf416b462552db562e5917b
kwaneung <kwaneung.kim@outlook.com>
2024-08-20 12:24:54 +0900

    feat: @next/bundle-analyzer 셋팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
 const nextConfig = {};
 
-export default nextConfig;
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default bundleAnalyzer(nextConfig);
