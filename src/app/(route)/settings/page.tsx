374d20a6da1dcfb1b41e5a9117691f6dee56bada
kwaneung <kwaneung.kim@outlook.com>
2024-08-26 22:33:23 +0900

    feat: Settings 하위 페이지 퍼블리싱

--- src/app/(route)/settings/page.tsx
+++ src/app/(route)/settings/page.tsx
@@ -1,17 +1,5 @@
-import { Separator } from '@/components/ui/separator';
-import { ProfileForm } from './profile-form';
+import { redirect } from 'next/navigation';
 
 export default function SettingsProfilePage() {
-  return (
-    <div className="space-y-6">
-      <div>
-        <h3 className="text-lg font-medium">Settings</h3>
-        <p className="text-sm text-muted-foreground">
-          This is how others will see you on the site.
-        </p>
-      </div>
-      <Separator />
-      <ProfileForm />
-    </div>
-  );
+  redirect('/settings/profile');
 }
---
+++ src/app/(route)/settings/profile/page.tsx
@@ -0,0 +1,17 @@
+import { Separator } from '@/components/ui/separator';
+import { ProfileForm } from './profile-form';
+
+export default function Profile() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Settings</h3>
+        <p className="text-sm text-muted-foreground">
+          This is how others will see you on the site.
+        </p>
+      </div>
+      <Separator />
+      <ProfileForm />
+    </div>
+  );
+}
