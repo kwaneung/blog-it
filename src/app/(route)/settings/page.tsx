1ba32ffb2267916dd366c22830d746d16f08fdd2
kwaneung <kwaneung.kim@outlook.com>
2024-08-26 12:51:09 +0900

    feat: Settings 퍼블리싱

--- src/app/(route)/settings/page.tsx
+++ src/app/(route)/settings/page.tsx
@@ -1,3 +1,17 @@
-export default function Settings() {
-  return <>Settings</>;
+import { Separator } from '@/components/ui/separator';
+import { ProfileForm } from './profile-form';
+
+export default function SettingsProfilePage() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Settings</h3>
+        <p className="text-sm text-muted-foreground">
+          This is how others will see you on the site.
+        </p>
+      </div>
+      <Separator />
+      <ProfileForm />
+    </div>
+  );
 }
