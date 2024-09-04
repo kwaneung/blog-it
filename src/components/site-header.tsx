88fa76e1571ce99513d275a2c23a50917992fed3
kwaneung <kwaneung.kim@outlook.com>
2024-09-04 12:12:17 +0900

    feat: 로그인 화면 Dialog 추가 완료

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -8,6 +8,18 @@ import { HeaderNav } from '@/components/header-nav';
 import { MobileNav } from '@/components/mobile-nav';
 import { ModeToggle } from '@/components/mode-toggle';
 import { Button, buttonVariants } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
 import { UserNav } from './user-nav';
 
 export function SiteHeader() {
@@ -40,9 +52,36 @@ export function SiteHeader() {
             {isLogin ? (
               <UserNav />
             ) : (
-              <Button className="h-8 ml-2" variant="outline">
-                로그인
-              </Button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="outline" className="h-8 ml-2">
+                    로그인
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>로그인</DialogTitle>
+                    <DialogDescription>로그인 정보를 입력 해주세요.</DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="name" className="text-right">
+                        ID
+                      </Label>
+                      <Input id="name" placeholder="ID" className="col-span-3" />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="username" className="text-right">
+                        Password
+                      </Label>
+                      <Input id="username" placeholder="Password" className="col-span-3" />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <Button type="submit">Login</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
             )}
           </nav>
         </div>
