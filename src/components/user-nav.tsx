10f082dd2845ab1b3a72c033ab4c7f6e1fc9244e
kwaneung <kwaneung.kim@outlook.com>
2024-10-14 23:15:42 +0900

    fix: 사용자 이미지 부분 추가

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -23,7 +23,13 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            <AvatarImage
+              src={user?.user_metadata.avatar_url}
+              alt="@shadcn"
+              style={{
+                objectFit: 'cover',
+              }}
+            />
             <AvatarFallback>SC</AvatarFallback>
           </Avatar>
         </Button>
