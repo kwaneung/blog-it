0fe557c7676736eb9930bb849b24e70623b1c3d8
kwaneung <kwaneung.kim@outlook.com>
2024-10-15 12:40:08 +0900

    fix: 세션이 존재하면 / 리다이렉트

--- src/app/(route)/auth/page.tsx
+++ src/app/(route)/auth/page.tsx
@@ -1,12 +1,23 @@
 // app/auth/page.tsx
 'use client';
 
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 const SignIn = () => {
   const supabaseClient = useSupabaseClient();
+  const session = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      router.push('/'); // Redirect to the root page if the user is already logged in
+    }
+  }, [session, router]);
 
   return (
     <div className="h-full flex justify-center items-center">
