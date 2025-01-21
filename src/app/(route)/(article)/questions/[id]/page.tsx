7b52c6364960798b1445e06623936c43e5426e7b
kwaneung <kwaneung.kim@outlook.com>
2025-01-21 12:25:00 +0900

    fix: vercel build error fix

--- src/app/(route)/(article)/questions/[id]/page.tsx
+++ src/app/(route)/(article)/questions/[id]/page.tsx
@@ -1,13 +1,14 @@
-// import { fetchPosts } from '@/services/post';
-// import ArticleDetail from '../../_components/post-detail';
-// import { IPost } from '@/types/post';
-// import { fetchUserProfile } from '@/services/userProfile';
+import { fetchPosts } from '@/services/post';
+import ArticleDetail from '../../_components/post-detail';
+import { IPost } from '@/types/post';
+import { fetchUserProfile } from '@/services/userProfile';
 
-// export default async function PostDetail({ params }: { params: IPost }) {
-//   const posts = await fetchPosts();
-//   const filteredPost = posts?.find((post: IPost) => post.id === params.id);
+export default async function PostDetail({ params }: { params: IPost }) {
+  const posts = await fetchPosts();
+  const filteredPost = posts?.find((post: IPost) => post.id === params.id);
 
-//   const { name: ownerName } = await fetchUserProfile(filteredPost?.user_id);
+  const { name: ownerName } = await fetchUserProfile(filteredPost?.user_id);
 
-//   return <ArticleDetail post={filteredPost} type={'questions'} ownerName={ownerName} />;
-// }
+  // return <ArticleDetail post={filteredPost} type={'questions'} ownerName={ownerName} />;
+  return <div>questions</div>;
+}
